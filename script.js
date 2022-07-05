3409339bf2133b0a438c866b243af7b046e4935c
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-05 19:26:27 -0300

    Desafio 2 concluido

--- script.js
+++ script.js
@@ -2,9 +2,24 @@ function inputText() {
   const textInput = document.getElementById('text-input');
   const pText = document.getElementById('meme-text');
 
-  textInput.addEventListener('keyup', function (){
-    pText.innerText = textInput.value
-  })
+  textInput.addEventListener('keyup', function () {
+    pText.innerText = textInput.value;
+  });
 }
 
 inputText();
+
+function fileImage() {
+  const imageFile = document.getElementById('meme-insert');
+  const conteiner = document.getElementById('meme-image');
+
+  
+imageFile.onchange = evt => {
+    const [file] = imageFile.files
+    if (file) {
+      conteiner.src = URL.createObjectURL(file)
+    }
+  }
+}
+
+fileImage();
