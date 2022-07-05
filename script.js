52d549901a7ccd7ac3e165cd26e4efde2afaac4c
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-05 20:47:06 -0300

    Requisito 6 concluito

--- script.js
+++ script.js
@@ -13,13 +13,30 @@ function fileImage() {
   const imageFile = document.getElementById('meme-insert');
   const conteiner = document.getElementById('meme-image');
 
-  
-imageFile.onchange = evt => {
-    const [file] = imageFile.files
+  imageFile.onchange = (evt) => {
+    const [file] = imageFile.files;
     if (file) {
-      conteiner.src = URL.createObjectURL(file)
+      conteiner.src = URL.createObjectURL(file);
     }
-  }
+  };
 }
 
 fileImage();
+
+function border() {
+  const fireButton = document.getElementById('fire');
+  const conteiner = document.getElementById('meme-image-container');
+  fireButton.addEventListener('click', function () {
+    conteiner.style.border = '3px dashed rgb(255, 0, 0)';
+  });
+  const waterButton = document.getElementById('water');
+  waterButton.addEventListener('click', function () {
+    conteiner.style.border = '5px double rgb(0, 0, 255)';
+  });
+  const earthButton = document.getElementById('earth');
+  earthButton.addEventListener('click', function () {
+    conteiner.style.border = '6px groove rgb(0, 128, 0)';
+  });
+}
+
+border();
